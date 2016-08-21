394bb0f59c1c2d539ab261616559f21666a0b741
Derek Fan <haoping.fan@gmail.com>
2016-08-21 13:53:43 +0800

    the object module completed

---
+++ object_oriented_programming_with_ruby/the_object_model/exercise1.rb
@@ -0,0 +1,12 @@
+=begin
+first, we have to define a class.
+
+Then we instantiate an obejct of that class 
+by .new method and save it to a variable.
+=end
+
+
+class GoodDog
+end
+
+lucky = GoodDog.new
